02589d5cb32340cbfa94f8477d218bc0a9230bd6
jeffreyd3308 <jeffreyd3308@nycstudents.net>
2024-10-01 15:42:02 +0000

    thirdcommit

--- src/main/java/com/example/project/StudentRunner.java
+++ src/main/java/com/example/project/StudentRunner.java
@@ -1,5 +1,10 @@
+package com.example.project;
+
 public class StudentRunner{
     public static void main(String[] args) {
         //use this StudentRunner class if you need to test your code 
+        Student s = new Student("John", "Doe", 2025);
+        s.getFirstName();
+        s.printStudentInfo();
     }
 }
